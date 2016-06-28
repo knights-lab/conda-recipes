eb3f2cc99bf784d35ac52af3d6c11bbb569b52ba
Benjamin Hillmann <bhillmann@outlook.com>
2016-06-28 12:14:25 -0500

    Simplified the build.sh

--- recipes/ninja_ops/build.sh
+++ recipes/ninja_ops/build.sh
@@ -8,8 +8,5 @@ $PYTHON setup.py install
 # http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build process.
 mkdir -p $PREFIX/bin
-
-binaries="bin"
-
-for f in $binaries/*; do cp $f $PREFIX/bin; done
+cp bin/* $PREFIX/bin/
 chmod +x $PREFIX/bin/*
