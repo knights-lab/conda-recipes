5fe6cd2e43f47f50c68efa67283127ff1eca7638
Benjamin Hillmann <bhillmann@outlook.com>
2016-06-28 12:06:48 -0500

    Added the recipe.

--- recipes/ninja_ops/bld.bat
+++ recipes/ninja_ops/bld.bat
@@ -7,5 +7,5 @@ if errorlevel 1 exit 1
 :: http://docs.continuum.io/conda/build.html
 :: for a list of environment variables that are set during the build process.
 
-xcopy bin\*.exe %LIBRARY_BIN%\
-if errorlevel 1 exit 1
+mkdir %SCRIPTS%
+xcopy bin\*.exe %SCRIPTS%
