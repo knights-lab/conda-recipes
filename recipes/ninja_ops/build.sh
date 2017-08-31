1f1d83221997c601e41c19f7af89ea5adbe512f7
bhillmann <bhillmann@outlook.com>
2017-08-31 14:14:38 -0500

    Updated for the new ninja_ops.

--- recipes/ninja_ops/build.sh
+++ recipes/ninja_ops/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$PYTHON setup.py install
+#$PYTHON setup.py install
 
 # Add more build steps here, if they are necessary.
 
@@ -8,5 +8,6 @@ $PYTHON setup.py install
 # http://docs.continuum.io/conda/build.html
 # for a list of environment variables that are set during the build process.
 mkdir -p $PREFIX/bin
+echo "Hello, World"
 cp bin/* $PREFIX/bin/
 chmod +x $PREFIX/bin/*
